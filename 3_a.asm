7fdcd695335e24e794d7a7ff533b2fc9f7bcca59
arnabcs17b006 <46973323+arnabcs17b006@users.noreply.github.com>
2019-03-29 15:44:22 +0530

    Update 3_a.asm

--- 3_a.asm
+++ 3_a.asm
@@ -1,49 +1,46 @@
-.data
-arr_1: .space 100
-n: .word 25
-arr_2: .space 100
-m: .word 25
-.text
-main:
-  lw $s0,n
-  lw $s1,m
-  la $s2,arr_1
-  la $s3,arr_2
-  move $s4,$s2
-  move $s5,$s3
-  li $t0,0
-  li $t1,0
-  li $t2,0
-  li $t3,0
-  
-L1:
-    beq $t0,$s0,L2
-    sw $s2,($s2)
-    sw $t2,0($s2)
-    sw $s3,($s3)
-    sw $t3,0($s3)
-    addi $t2,$t2,1
-    addi $t3,$t3,2
-    addi $s2,$s2,4
-    addi $s3,$s3,4
-    addi $t0,$t0,1
-    b L1
-    
- L2:
-    beq $t1,$s0,E
-    lw $t4,0($s0)
-    lw $t5,0($s1)
-    sw $t5,$s0
-    sw $t4,$s1
-    addi $s0,$s0,4
-    addi $s1,$s1,4
-    addi $t1,$t1,1
-    b L2
-    
- 
-    
-  
-  
-  E:
-  j $31
-j $31
+       .data
+first_arr:    .space 100           
+size1:  .word  5             
+second_arr:    .space 100
+size2:  .word  5
+   
+        .text
+main:   
+        lw   $s0,size1                          
+        lw   $s1,size2                          
+        la   $s2,first_arr                      
+        la   $s3,second_arr                      
+        move $s4,$s2                            
+        move $s5,$s3                            
+        li   $t0,0                              
+        li   $t1,0                             
+        li   $t2,1                              
+        li   $t3,2                             
+
+L1: 
+        beq  $t0,$s0,L2                       
+        sw   $s2,($s2)                          
+        sw   $t2,0($s2)                         
+        sw   $s3,($s3)       
+        sw   $t3,0($s3)                         
+        addi $t2,$t2,6                          
+        addi $t3,$t3,7                          
+        addi $s2,$s2,4                          
+        addi $s3,$s3,4                          
+        addi $t0,$t0,1                          
+        b L1                                   
+
+L2:   
+        beq  $t1,$s0,E                      
+        lw $t4,0($s4)                           
+        sw $s5,0($s4)                          
+        lw $t5,0($s5)                           
+        sw $t4,0($s5)                           
+        addi $s4,$s4,4                         
+        addi $s5,$s5,4                         
+        addi $t1,$t1,1                         
+        b L2                            
+
+
+E:
+jr $ra 
