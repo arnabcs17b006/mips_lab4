0b2d7a66060df36d5208e9d48881fb4e6ad71bd8
arnabcs17b006 <46973323+arnabcs17b006@users.noreply.github.com>
2019-03-29 15:32:23 +0530

    Update 1_2.asm

--- 1_2.asm
+++ 1_2.asm
@@ -1,23 +1,10 @@
 main:
-  addi $sp,$sp,-16
-  sw $s0,12($sp)
-  sw $s1,8($sp)
-  sw $s2,4($sp)
-  sw $s3,0($sp)
-  li $s0, 9
-  li $s1, 0
-  li $s2, 0
-  li $s3, 0
-  
-  srl   $s0,$s0,31     
-  andi  $s1,$s0,1
-  bne   $s1,$s2,EXIT
-  EXIT:
-  addi $s3,$s3,0
-  lw $s0,12($sp)
-  lw $s1,8($sp)
-  lw $s2,4($sp)
-  lw $s3,0($sp)
-  j $31
-  
-  
+	addi $sp,$sp,-8
+	sw   $s0,4($sp)
+	sw   $s1,0($sp)
+	addi $s0,$zero,-5
+	srl  $s1,$s0,31
+	lw   $s1,0($sp)
+	lw   $s0,4($sp)
+	addi $sp,$sp,8
+j $31
