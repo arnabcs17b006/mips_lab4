9f06aa5c08efb372e9197596779ec084fa18506d
arnabcs17b006 <46973323+arnabcs17b006@users.noreply.github.com>
2019-03-29 15:34:05 +0530

    Update 1_3.asm

--- 1_3.asm
+++ 1_3.asm
@@ -1,62 +1,48 @@
 main:
-	addi $sp,$sp,-36
-	sw $s0,32($sp)
-	sw $s1,28($sp)
-	sw $s2,24($sp)
-	sw $s3,20($sp)
-	sw $s4,16($sp)
-	sw $s5,12($sp)
-	sw $s6,8($sp)
-	sw $s7,4($sp)
-	sw $s8,0($sp)
-	li $s0,76
-	li $s1,0
-	li $s2,0
-	li $s3,0
-	li $s4,0
-	li $s5,0
-	li $s6,1
-	li $s8,0
-	
-	andi $s3,$s0.$1
-	beq $s3,$s6,L1
-	beq $s3,$s5,L0
-	
-	L0:
-		addi $s1,$s1,1
-		srl  $s0,$s0,1
-		andi $s7,$s0,1
-		beq $s7,$s6,L1
-		
-		addi $s1,$s1,-1
-		beq $s0,0,END
-		beq $s7,$s5,L0
-		
-	L1:
-		addi $s2,$s2,1
-		srl  $s0,$s0,1
-     		andi $s8,$s0,1
-       		beq  $s8,$s5,L0
-      
-       		addi $s2,$s2,-1
-       		beq  $s0,$zero,E
-       		beq  $s8,$s6,L1
+	addi $sp,$sp,4
+	sw   $s0,0($sp) 
+	addi $s0,$zero,0xFFFF
+	move $t2,$s0
+	addi $t0,$zero,0
+	addi $t1,$zero,0
+	addi $t3,$zero,0
+	addi $t4,$zero,0
+	addi $t5,$zero,0
+	addi $t6,$zero,0
+	andi $t3,$t2,1
+	slti $t0,$t3,1
+	slt  $t1,$zero,$t3
+	srl  $t2,$t2,1
+	j    L1
 
+L1:
+	blt  $t5,31,L2
+	j    L5
 
-	E:
-		lw $s8,0($sp)
-		lw $s7,4($sp)
-		lw $s6,8($sp)
-		lw $s5,12($sp)
-		lw $s4,16($sp)
-		lw $s3,20($sp)
-		lw $s2,24($sp)
-		lw $s1,28($sp)
-		lw $s0,32($sp)
-		addi $sp,$sp,36
-		j $31
-	
-		
+L2:
+	andi $t4,$t2,1
+	beq  $t4,1,L3
+	beq  $t4,0,L4
+
+L3:
+	slt  $t6,$t3,$t4
+	add  $t1,$t1,$t6
+	move $t3,$t4
+	srl  $t2,$t2,1
+	addi $t5,$t5,1
+	j    L1
+
+L4:
+	slt  $t6,$t4,$t3
+	add  $t0,$t0,$t6
+	move $t3,$t4
+	srl  $t2,$t2,1
+	addi $t5,$t5,1
+	j    L1
+
+L5:    	lw   $s0,0($sp)
+	addi $sp,$sp,8
+j $31
 		
 	 
 	
