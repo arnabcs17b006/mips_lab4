23f0a42af91a14896069dc7eab2d5e60ce40ff2d
arnabcs17b006 <46973323+arnabcs17b006@users.noreply.github.com>
2019-04-11 22:22:16 +0530

    Update 3_a.asm

--- 3_a.asm
+++ 3_a.asm
@@ -1,21 +1,21 @@
        .data
-first_arr:    .space 20        
+arr1:    .space 20        
 size1:  .word  5             
-second_arr:    .space 20
+arr2:    .space 20
 size2:  .word  5
    
         .text
 main:   
         lw   $s0,size1                          
         lw   $s1,size2                          
-        la   $s2,first_arr                      
-        la   $s3,second_arr                      
+        la   $s2,arr1                      
+        la   $s3,arr2                      
         move $s4,$s2                            
         move $s5,$s3                            
-        li   $t0,0                              
-        li   $t1,0                             
-        li   $t2,1                              
-        li   $t3,2                             
+        addi   $t0,$zero,0                              
+        addi   $t1,$zero,-1                             
+        addi   $t2,$zero,1                              
+        addi   $t3,$zero,2                             
 
 L1: 
         beq  $t0,$s0,L2                       
@@ -23,12 +23,12 @@ L1:
         sw   $t2,0($s2)                         
         sw   $s3,($s3)       
         sw   $t3,0($s3)                         
-        addi $t2,$t2,6                          
-        addi $t3,$t3,7                          
+        addi $t2,$t2,1                          
+        addi $t3,$t3,2                          
         addi $s2,$s2,4                          
         addi $s3,$s3,4                          
         addi $t0,$t0,1                          
-        b L1                                   
+        j L1                                   
 
 L2:   
         beq  $t1,$s0,E                      
@@ -39,8 +39,8 @@ L2:
         addi $s4,$s4,4                         
         addi $s5,$s5,4                         
         addi $t1,$t1,1                         
-        b L2                            
+        j L2                            
 
 
 E:
-jr $ra 
+j $ra 
